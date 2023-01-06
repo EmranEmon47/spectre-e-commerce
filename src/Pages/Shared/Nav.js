d96c83da0e545b549da55298e36176a2d1cdc95f
EmranEmon47 <aa.emran47@gmail.com>
2023-01-06 23:22:04 +0600

    nav bar set

--- src/Pages/Shared/Nav.js
+++ src/Pages/Shared/Nav.js
@@ -2,83 +2,36 @@ import React from "react";
 
 export const Nav = () => {
   return (
-    <div>
-      <div className="navbar bg-base-100">
-        <div className="navbar-start">
-          <div className="dropdown">
-            <label tabIndex={0} className="btn btn-ghost lg:hidden">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="w-5 h-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h8m-8 6h16"
-                />
-              </svg>
-            </label>
-            <ul
-              tabIndex={0}
-              className="p-2 mt-3 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52"
-            >
-              <li>
-                <a>Item 1</a>
-              </li>
-              <li tabIndex={0}>
-                <a className="justify-between">
-                  Parent
-                  <svg
-                    className="fill-current"
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" />
-                  </svg>
-                </a>
-                <ul className="p-2">
-                  <li>
-                    <a>Submenu 1</a>
-                  </li>
-                  <li>
-                    <a>Submenu 2</a>
-                  </li>
-                </ul>
-              </li>
-              <li>
-                <a>Item 3</a>
-              </li>
-            </ul>
-          </div>
-          <a className="text-xl normal-case btn btn-ghost">Spectre</a>
+    <main>
+      <div className="w-full h-[100px] px-[120px] flex justify-between items-center">
+
+        {/* logo */}
+        <div>
+          <h1 className="text-4xl font-bold">Spectre</h1>
         </div>
-        <div className="hidden navbar-center lg:flex">
+
+        {/* search filter */}
+        <div>
           <div className="form-control">
-            <div className="input-group">
-              <select className="text-white select bg-primary select-bordered">
-                <option className="text-white bg-white"  disabled selected>
+            <div className="input-group h-[40px]">
+              <select className="px-2 py-2 text-white w-14 bg-primary">
+                <option className="text-black bg-white" disabled selected>
                   All
                 </option>
-                <option className="text-black bg-white border-none">T-shirts</option>
-                <option className="text-black bg-white border-none">Mugs</option>
-                <option className="text-black bg-white border-none">Mugs</option>
-                <option className="text-black bg-white border-none">Mugs</option>
+                <option className="text-black bg-white">Game of Thrones</option>
+                <option className="text-black bg-white">Lost</option>
+                <option className="text-black bg-white">Breaking Bad</option>
+                <option className="text-black bg-white">Walking Dead</option>
               </select>
               <input
                 type="text"
                 placeholder="Search…"
-                className="input input-bordered"
+                className="input input-bordered w-[516px] h-full"
               />
-              <button className="btn bg-primary btn-square">
+              <button className="h-full px-2 py-2 text-white bg-primary">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
-                  className="w-6 h-6 text-white "
+                  className="w-6 h-6 "
                   fill="none"
                   viewBox="0 0 24 24"
                   stroke="currentColor"
@@ -94,10 +47,108 @@ export const Nav = () => {
             </div>
           </div>
         </div>
-        <div className="navbar-end">
-          <a className="text-white border-none btn bg-primary">Get started</a>
+
+        {/* login-compare-register */}
+        
+        <div className="flex items-center justify-between ">
+          <div className="flex items-center justify-between px-4">
+            <div className="flex flex-col items-center justify-center pr-1">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z"
+                />
+              </svg>
+              <p className="pt-1 text-xs">Login</p>
+            </div>
+            <div className="flex flex-col items-center justify-center px-1">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
+                />
+              </svg>
+
+              <p className="pt-1 text-xs">Register</p>
+            </div>
+
+            <div className="flex flex-col items-center justify-center px-1">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
+                />
+              </svg>
+
+              <p className="pt-1 text-xs">Wishlist</p>
+            </div>
+
+            <div className="flex flex-col items-center justify-center px-1">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M7.5 21L3 16.5m0 0L7.5 12M3 16.5h13.5m0-13.5L21 7.5m0 0L16.5 12M21 7.5H7.5"
+                />
+              </svg>
+
+              <p className="pt-1 text-xs">Compare</p>
+            </div>
+          </div>
+
+          {/* shopping cart */}
+          <div className="flex items-center justify-between">
+            <p className="px-2 text-sm">0 items: 00.00 $</p>
+            <button className="text-white h-[40px] px-2 rounded py-2 bg-primary ">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"
+                />
+              </svg>
+            </button>
+          </div>
         </div>
       </div>
-    </div>
+    </main>
   );
 };
